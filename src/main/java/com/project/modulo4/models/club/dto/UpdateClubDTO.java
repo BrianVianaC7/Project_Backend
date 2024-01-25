6a7936c54984d310c7d97c4f0dc390ac5471fd9c
Brian <Brian.vianac@outlook.com>
2024-01-24 19:22:41 -0600

    adding validations

--- src/main/java/com/project/modulo4/models/club/dto/UpdateClubDTO.java
+++ src/main/java/com/project/modulo4/models/club/dto/UpdateClubDTO.java
@@ -1,13 +1,21 @@
 package com.project.modulo4.models.club.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateClubDTO {
 
+    @NotBlank(message = "El nombre del club no puede estar en blanco")
     private String clubName;
+
+    @NotBlank(message = "El nombre corto del club no puede estar en blanco")
     private String clubShortName;
+
+    @NotBlank(message = "El nombre del estadio no puede estar en blanco")
     private String stadium;
+
+    @NotBlank(message = "La URL de la imagen del club no puede estar en blanco")
     private String clubImage;
 
 }
--- src/main/java/com/project/modulo4/models/player/dto/UpdatePlayerDTO.java
+++ src/main/java/com/project/modulo4/models/player/dto/UpdatePlayerDTO.java
@@ -1,7 +1,9 @@
 package com.project.modulo4.models.player.dto;
 
 import com.project.modulo4.utils.types.PositionTypes;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +15,8 @@ public class UpdatePlayerDTO {
     private String longName;
     @NotBlank
     private String nickName;
+    @Min(value = 1, message = "El dorsal tiene que ser mayor a 0")
     private int numberJersey;
+    @NotNull(message = "La posición no puede estar en blanco")
     private PositionTypes position;
 }
