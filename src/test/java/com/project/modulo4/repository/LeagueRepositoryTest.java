0a3824a0f3f07cfcf618617d29bb68a539e7699a
BrianVianaC <A340378836@my.uvm.edu.mx>
2024-02-10 20:10:10 -0600

    add LeagueRepositoryTest and PlayerRepositoryTest

--- src/test/java/com/project/modulo4/repository/LeagueRepositoryTest.java
+++ src/test/java/com/project/modulo4/repository/LeagueRepositoryTest.java
@@ -1,7 +1,40 @@
 package com.project.modulo4.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.project.modulo4.models.club.model.ClubModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class LeagueRepositoryTest {
 
+    @Mock
+    private LeagueRepository leagueRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindClubsByLeague() {
+        long leagueId = 1L;
+        List<ClubModel> clubModels = new ArrayList<>();
+        ClubModel clubModel = new ClubModel();
+
+        clubModels.add(clubModel);
+
+        when(leagueRepository.findClubsByLeague(leagueId)).thenReturn(clubModels);
+        List<ClubModel> result = leagueRepository.findClubsByLeague(leagueId);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(clubModel, result.get(0));
+    }
 }
--- src/test/java/com/project/modulo4/repository/PlayerRepositoryTest.java
+++ src/test/java/com/project/modulo4/repository/PlayerRepositoryTest.java
@@ -1,7 +1,33 @@
 package com.project.modulo4.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class PlayerRepositoryTest {
 
+    @Mock
+    private PlayerRepository playerRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindMaxPlayerId() {
+        // Given
+        Long maxId = 100L;
+        when(playerRepository.findMaxPlayerId()).thenReturn(maxId);
+
+        // When
+        Long result = playerRepository.findMaxPlayerId();
+
+        // Then
+        assertNotNull(result);
+        assertEquals(maxId, result);
+    }
 }
